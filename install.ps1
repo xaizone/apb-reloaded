2b36a0af4321f3d585a4e83851a2e6079d42e362
xaizone <git@xaizone.eu>
2024-11-02 21:07:38 +0100

    refactor: small ref with install script

--- install.ps1
+++ install.ps1
@@ -1,5 +1,4 @@
 git clone --bare https://github.com/xaizone/apb-reloaded.git $pwd/.config
-function config { git --git-dir=$pwd/.config/ --work-tree=$pwd $args }
-config checkout -f
-config config status.showUntrackedFiles no
 git config --global --replace-all alias.apb-cfg "!git --git-dir=\`"$pwd\.config\`" --work-tree=\`"$pwd\`""
+git apb-cfg checkout -f
+git apb-cfg config status.showUntrackedFiles no

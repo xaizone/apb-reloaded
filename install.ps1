5168754a79429e812ea66273e84fff633819a743
xaizone <git@xaizone.eu>
2025-07-31 10:18:32 +0200

    fix: added new gui element after new update, leftover github

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
-git clone --bare https://github.com/xaizone/apb-reloaded.git $pwd/.config
+git clone --bare https://git.xaizone.eu/xaizone/apb-reloaded.git $pwd/.config
 git config --global --replace-all alias.apb-cfg "!git --git-dir=\`"$pwd\.config\`" --work-tree=\`"$pwd\`""
 git apb-cfg checkout -f
 git apb-cfg config status.showUntrackedFiles no
